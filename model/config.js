2053ca5e7db45a4fcd5efddef121ccf488ff116b
Cat-bl <32174@qq.com>
2026-05-08 16:48:26 +0800

    fix

--- model/config.js
+++ model/config.js
@@ -8,7 +8,8 @@ class RConfig extends Base{
     constructor(e) {
         super(e);
         this.configPath = `./plugins/${RConfig.pluginName}/config/`;
-        this.previousContent = new Map(); // 存储每个文件之前的内容
+        this.previousContent = new Map();
+        this.watchers = new Map();
     }
 
     getConfig(name) {
@@ -37,8 +38,8 @@ class RConfig extends Base{
         let file = this.getFilePath(name);
         const yaml = YAML.parse(fs.readFileSync(file, 'utf8'));
 
+        this.previousContent.set(name, yaml);
         if (isWatch) {
-            this.previousContent.set(name, yaml); // 保存初始内容
             this.watch(file, name);
         }
         return yaml;
@@ -49,28 +50,55 @@ class RConfig extends Base{
     }
 
     watch(file, name) {
-        const watcher = chokidar.watch(file);
+        if (this.watchers.has(name)) return;
+
+        const watcher = chokidar.watch(file, {
+            ignoreInitial: true
+        });
 
         watcher.on('change', path => {
-            const currentContent = YAML.parse(fs.readFileSync(path, 'utf8'));
-            const previousContent = this.previousContent.get(name);
+            try {
+                const currentContent = YAML.parse(fs.readFileSync(path, 'utf8'));
+                const previousContent = this.previousContent.get(name);
 
-            if (!_.isEqual(previousContent, currentContent)) {
-                logger.mark(`[bl-chat-plugin][配置文件]：${name}已经被重置`);
-                this.previousContent.set(name, currentContent); // 更新之前的内容
+                if (!_.isEqual(previousContent, currentContent)) {
+                    logger.mark(`[bl-chat-plugin][配置文件]：${name}已被重新加载`);
+                    this.previousContent.set(name, currentContent);
+                }
+            } catch (error) {
+                logger.error(`[bl-chat-plugin][配置文件]：${name}读取失败`, error);
             }
         });
+
+        watcher.on('unlink', () => {
+            this.previousContent.delete(name);
+        });
+
+        watcher.on('error', error => {
+            logger.error(`[bl-chat-plugin][配置文件]：${name}监听失败`, error);
+        });
+
+        this.watchers.set(name, watcher);
     }
 
     saveAllConfig(name, data) {
         let file = this.getFilePath(name);
         if (_.isEmpty(data)) {
             fs.existsSync(file) && fs.unlinkSync(file);
+            this.previousContent.delete(name);
         } else {
             let yaml = YAML.stringify(data);
             fs.writeFileSync(file, yaml, 'utf8');
+            this.previousContent.set(name, data);
         }
         this.watch(file, name);
+    }
+
+    async closeWatchers() {
+        for (const watcher of this.watchers.values()) {
+            await watcher.close().catch(() => {});
+        }
+        this.watchers.clear();
     }
 }
 
